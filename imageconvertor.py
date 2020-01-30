2c14c94ca07160f374cb9167db59c58d2400ab93
Shruti Harrison <51241036+ShrutiHarrison@users.noreply.github.com>
2020-01-30 22:04:59 +0530

    Update imageconvertor.py

--- imageconvertor.py
+++ imageconvertor.py
@@ -2,6 +2,6 @@ from PIL import Image  #Image is required to load input image in PIL format
 import pytesseract     #pytesseract is used to recognise the text from image 
 
 pytesseract.pytesseract.tesseract_cmd='C:\\Program Files\\Tesseract-OCR\\tesseract.exe' #specifying the path to the tesseract.exe file
-img = Image.open("image2.jpg") #accessing the image 
+img = Image.open("image.jpg") #accessing the image 
 output_text = pytesseract.image_to_string(img,lang= 'eng') #converting the image contents to string
 print(output_text) #printing the image contents in terminal
