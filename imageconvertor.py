758905e32794ec53d8bde78dd1ee15517d482764
Shruti Harrison <51241036+ShrutiHarrison@users.noreply.github.com>
2020-01-30 21:46:11 +0530

    Add files via upload

---
+++ imageconvertor.py
@@ -0,0 +1,7 @@
+from PIL import Image  #Image is required to load input image in PIL format
+import pytesseract     #pytesseract is used to recognise the text from image 
+
+pytesseract.pytesseract.tesseract_cmd='C:\\Program Files\\Tesseract-OCR\\tesseract.exe' #specifying the path to the tesseract.exe file
+img = Image.open("image2.jpg") #accessing the image 
+output_text = pytesseract.image_to_string(img,lang= 'eng') #converting the image contents to string
+print(output_text) #printing the image contents in terminal
